f64aa7e0ce5bd3c2bf78c2f1a903699f2f18df4e
airdropbomb <saginaw135@gmail.com>
2025-03-24 22:18:43 +0630

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+export const config = {
+  baseURL: "https://referralapi.layeredge.io/api",
+  ref_code: "amR1ncRj",
+  num_ref: 500, // number of references
+  max_threads: 50, // number of threads
+  auto_connect_twitter: false,
+  auto_task: false,
+
+  ////// Test Key============Do not change
+  rpc: "https://1rpc.io/base",
+  // valid values: 2captcha, anticaptcha
+  TYPE_CAPTCHA: "2captcha",
+  API_KEY_2CAPTCHA: "xxx", // Go to: https://2captcha.com/
+  API_KEY_ANTI_CAPTCHA: "xxx", // Go to: https://getcaptchasolution.com/bdavzy2dxw
+  CAPTCHA_URL: "https://dashboard.layeredge.io/",
+  WEBSITE_KEY: "6LdS4sEqAAAAAPCYk08sXJmhem7o0hKVxz3s0jDS",
+};
